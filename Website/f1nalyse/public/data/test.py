ae9700e3dd788f401445910383d66f421668da96
Kieran Daanoy <63813810+kdaanoy@users.noreply.github.com>
2026-06-16 09:25:07 +0100

    c

--- Website/f1nalyse/public/data/test.py
+++ Website/f1nalyse/public/data/test.py
@@ -1,10 +1,7 @@
 import fastf1
 
-session = fastf1.get_session(2026, "Shanghai", "Q")
+session = fastf1.get_session(2026, 6, "Q")
 session.load()
 results = session.results
 
-qualifying = {}
-for _, row in results.iterrows():
-    qualifying[row['FullName']] = int(row['Position'])
-print(qualifying)
+print(results)
